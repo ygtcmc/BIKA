0a63b7a22d14352361f6c8dfe911f5272d39468c
ygtcmc <92249364+ygtcmc@users.noreply.github.com>
2025-07-10 20:41:09 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.shizq.bika"
+        applicationId = "com.shizq.bika.a"
         minSdk = 24
         targetSdk = 34
         versionCode = 8
